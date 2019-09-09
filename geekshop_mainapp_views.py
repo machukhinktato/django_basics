2c80922308643e8fd360009749d4de5dcf18254d
machukhinktato <tarabrinmv@gmail.com>
2019-09-09 13:53:59 +0300

    hw2 complete
    there is hw 2, without extended task

--- geekshop_mainapp_views.py
+++ geekshop_mainapp_views.py
@@ -4,12 +4,34 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', context=content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+        'same_products': 'same_products'
+    }
+    return render(request, 'mainapp/products.html', context=content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', context=content)
+
+
+def content(request):
+    return render(request, 'mainapp/index.html', content)
