10d53c7e67854808898b8fbbdb1b6432dd5e2c6c
machukhinktato <tarabrinmv@gmail.com>
2019-09-10 02:47:33 +0300

    hw_reorganized_folders-added
    trying my best :)

--- geekshop_mainapp_models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
-    decription = models.TextField(verbose_name='описание', blank=True)
-
-    def __str__(self):
-        return self.name
