2459bb3fd43a11ad2b8110fdba1a0d9718193868
machukhinktato <tarabrinmv@gmail.com>
2019-09-19 16:33:07 +0300

    homework three
    сделал всю основную часть домашнего задания

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,17 +16,23 @@ Including another URLconf
 
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 
 import mainapp.views as mainapp
 
-
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('products/', mainapp.products, name='products'),
     path('products/home/', mainapp.products, name='products_home'),
     path('products/office/', mainapp.products, name='products_office'),
     path('products/modern/', mainapp.products, name='products_modern'),
     path('products/classic/', mainapp.products, name='products_classic'),
+    path('products/', mainapp.products, name='products'),
     path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
