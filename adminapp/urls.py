07d8b6b8ab67ab20c70a72761baef85cb1224c6f
machukhinktato <tarabrinmv@gmail.com>
2019-11-20 01:00:12 +0300

    hw 8 basic tasks complete
    1. work with products in adminapp
    2. paginator mode made
    3. CBV more realize
    4, filters added

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,7 @@
-from adminapp.views import user_delete, user_update, product_update, category_update, category_create, user_create, \
-    users, product_create, products, categories, category_delete, product_read, product_delete
+from adminapp.views import user_delete, user_update, product_update, user_create, \
+    product_create, products, categories, product_delete
 from django.urls import path
+import adminapp.views as adminapp
 
 
 app_name = 'adminapp'
@@ -8,18 +9,18 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('users/create/', user_create, name='user_create'),
-    path('users/read/', users, name='users'),
+    path('users/read/', adminapp.UsersListView.as_view(), name='users'),
     path('users/update/<int:pk>/', user_update, name='user_update'),
     path('users/delete/<int:pk>/', user_delete, name='user_delete'),
 
-    path('categories/create/', category_create, name='category_create'),
+    path('categories/create/', adminapp.ProductCategoryCreateView.as_view(), name='category_create'),
     path('categories/read/', categories, name='categories'),
-    path('categories/update/<int:pk>/', category_update, name='category_update'),
-    path('categories/delete/<int:pk>/', category_delete, name='category_delete'),
+    path('categories/update/<int:pk>/', adminapp.ProductCategoryUpdateView.as_view(), name='category_update'),
+    path('categories/delete/<int:pk>/', adminapp.ProductCategoryDeleteView.as_view(), name='category_delete'),
 
     path('products/create/category/<int:pk>/', product_create, name='product_create'),
     path('products/read/category/<int:pk>/', products, name='products'),
-    path('products/read/<int:pk>/', product_read, name='product_read'),
+    path('products/read/<int:pk>/', adminapp.ProductDetailView.as_view(), name='product_read'),
     path('products/update/<int:pk>/', product_update, name='product_update'),
     path('products/delete/<int:pk>/', product_delete, name='product_delete'),
 ]
